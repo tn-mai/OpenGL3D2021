3ad3d28d4eca4c51919ae0381c38a90c9fb6969d
田中 真依 <rabitfiver@gmail.com>
2021-04-07 03:51:48 +0900

    第01回のコードを実装

---
+++ Src/Main.cpp
@@ -0,0 +1,75 @@
+/**
+* @file Main.cpp
+*/
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <string>
+#include <iostream>
+#pragma comment(lib, "opengl32.lib")
+
+/**
+* OpenGLからのメッセージを処理する.
+*
+* @param source    メッセージの発信者(OpenGL、Windows、シェーダーなど).
+* @param type      メッセージの種類(エラー、警告など).
+* @param id        メッセージを一位に識別する値.
+* @param severity  メッセージの重要度(高、中、低、最低).
+* @param length    メッセージの文字数. 負数ならメッセージは0終端されている.
+* @param message   メッセージ本体.
+* @param userParam コールバック設定時に指定したポインタ.
+*/
+void GLAPIENTRY DebugCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam)
+{
+  if (length < 0) {
+    std::cerr << message << "\n";
+  } else {
+    const std::string s(message, message + length);
+    std::cerr << s << "\n";
+  }
+}
+
+/**
+* エントリーポイント.
+*/
+int main()
+{
+  // GLFWの初期化.
+  if (glfwInit() != GLFW_TRUE) {
+    return 1;
+  }
+
+  // 描画ウィンドウの作成.
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+  glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
+  GLFWwindow* window =
+    glfwCreateWindow(1280, 720, "OpenGLGame", nullptr, nullptr);
+  if (!window) {
+    glfwTerminate();
+    return 1;
+  }
+  glfwMakeContextCurrent(window);
+
+  // OpenGL関数のアドレスを取得する.
+  if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress))) {
+    glfwTerminate();
+    return 1;
+  }
+
+  glDebugMessageCallback(DebugCallback, nullptr);
+
+  // メインループ.
+  while (!glfwWindowShouldClose(window)) {
+    glClearColor(0.1f, 0.3f, 0.5f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glfwPollEvents();
+    glfwSwapBuffers(window);
+  }
+
+  // GLFWの終了.
+  glfwTerminate();
+
+  return 0;
+}
